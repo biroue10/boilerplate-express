a5266ae85145669fddd90c05487190a6785114db
BIROUE ISAAC <biroueisaac@gmail.com>
2023-06-19 20:50:25 +0100

    Intial Commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
+app.get("/",(req, res) => {
+    const path = __dirname + '/views/index.html'
+    res.sendFile(path)
+})
+app.use("/public", express.static(__dirname + '/public'))
 
-
-
+app.get("/json", (req,res)=>{
+    res.json({"message": "Hello json"})
+})
 
 
 
